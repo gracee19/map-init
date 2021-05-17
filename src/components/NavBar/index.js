9a76932a1740dc320bd9a40ea821804f465129e0
Grace Palenzuela <gracee_palenzuela@yahoo.com>
2021-05-17 19:03:49 +0930

    NavBar added

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const NavBar = () => (
+    <header className='navbar'>
+        <div className='navbar__title navbar__item'>MapInit</div>
+        <div className='navbar__item'>About Us</div>
+        <div className='navbar__item'>Contact</div>
+        <div className='navbar__item'>Help</div>        
+    </header>
+);
+
+export default NavBar;
